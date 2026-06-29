b4693169bffc39f3357942c69840b8ff4b4307d2
DEVIDPUTRA2000CC <pranaykapadia289@gmail.com>
2026-06-29 13:13:57 +0530

    submission-0.cpp

--- Data Structures & Algorithms/merge-sorted-array/submission-0.cpp
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.cpp
@@ -16,3 +16,14 @@ public:
         }
     }
 };
+
+// Alternate method->
+for(int i=0; i<m+n; i++){
+		if(i<m){
+		nums1[i]=nums1[i];
+	}if(i>=m){
+		nums1[i]=nums2[i-m];
+	}
+	} sort(nums1.begin(), nums1.end());
+    
+    } 
